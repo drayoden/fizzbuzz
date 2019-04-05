7b02ee3b0cd42f96ecc92e61adb3788db7280cae
Daryn Oden <daryn@hex50.com>
2019-04-05 12:47:23 -0700

    Update fizzbuzz03.py
    correct capital letters for "FizzBuzz"

--- fizzbuzz03.py
+++ fizzbuzz03.py
@@ -4,4 +4,5 @@
 
 print ("~~~ fizzbuzz ~~~")
 for i in range(1,101):
-    print("fizzbuzz") if (i % 15 == 0) else print("buzz") if (i % 5 == 0) else print("fizz") if (i % 3 == 0) else print(i)
+    print("FizzBuzz") if (i % 15 == 0) else print("Buzz") if (i % 5 == 0) else print("Fizz") if (i % 3 == 0) else print(i)
+FF
