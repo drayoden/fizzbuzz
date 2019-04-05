b8a6912c288d81317db066b168bf1ef89ed29e27
Daryn Oden <daryn@hex50.com>
2019-04-05 12:05:27 -0700

    Create fizzbuzz02.py

---
+++ fizzbuzz02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+print ("~~~ fizzbuzz ~~~")
+for i in range(1,101):
+    if (i % 15 == 0):
+        print("fizzbuzz")
+    elif (i % 5 == 0):
+        print("buzz")
+    elif (i % 3 == 0):
+        print("fizz")
+    else:
+        print(i)
